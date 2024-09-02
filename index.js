dd6ea7bb450dc3a4df25384110ff159200d1264c
MyNameIsGustavo <gustavo.professional.contact@gmail.com>
2024-09-02 05:35:34 -0300

    fix:: porta de servidor e liberacao de cors

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import Express from "express";
+import cors from "cors";
 import { visaoDasRotasDeProduto } from "./src/views/produtoView.js";
 import { visaoDasRotasDeUsuario } from "./src/views/usuarioView.js";
 
 const app = Express();
-const port = 3000;
+const port = 5000;
+
+app.use(cors());
 
 app.use(Express.json());
+
 
 app.use("/nativeCoffe", [
   visaoDasRotasDeUsuario,
